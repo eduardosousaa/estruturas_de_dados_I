df522bfd9aad5075eb0705cbdb119c2a60b9e649
Eduardo de Sousa <97894971+eduardosousaa@users.noreply.github.com>
2022-12-10 09:26:23 -0300

    up

--- alocacao/ex001.c
+++ alocacao/ex001.c
@@ -1,3 +1,8 @@
+/* 
+Um usuário vai informar uma série de números. O usuário não sabe quantos serão, só sabe que a condição de parada será -1. Ao final, deverá informar todos os números que o usuário digitou e métricas estatísticas como: 
+- média; 
+- desvio padrão;
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
