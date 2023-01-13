da196eb42bed482f0ce8084428c7f0d07a54730a
Eduardo de Sousa <97894971+eduardosousaa@users.noreply.github.com>
2023-01-12 21:00:03 -0300

    up

--- atividade-image/main.c
+++ atividade-image/main.c
@@ -11,14 +11,20 @@ int main(){
 	
 	/*
 	1 - Comente o que acontece com as linhas 15 e 16
+
+	// Esse primeiro não vai dar certo, pois não vai dar certo atribuir um valor ao pixels dentro do arquivo main.
 	img->pixels[10].valor = 255;
 	print("%d ", img->pixels[10].valor);
 	*/
+
 	/*
 	2 - Comente o que acontece com as linhas 20 e 21
+
+	//Esse segundo vai funcionar, pois ele está fazendo a chamada de uma função que está em outro arquivo de maneira correta.
 	setPixelValue(img, 3,2, 255);
 	printf("Valor da posicao 3,2 %d\n", getPixelValue(img, 3,2));
 	*/
+
 	liberarImagem(img);
 
 	return 0;
